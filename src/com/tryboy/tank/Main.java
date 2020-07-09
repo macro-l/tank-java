4eac832825443de2f1cce625ad79442a1210a93c
berserker-hong <786928688@qq.com>
2020-07-09 14:14:05 +0800

    键盘时间刷新窗口， 主线程循环刷新窗口

--- src/com/tryboy/tank/Main.java
+++ src/com/tryboy/tank/Main.java
@@ -1,49 +1,14 @@
 package com.tryboy.tank;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         TankFrame tf = new TankFrame();
 
+        while(true) {
+            Thread.sleep(50);
+            tf.repaint();
+        }
+
     }
 
 }
-
-/**
- * V.1版本
- */
-/*
-import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
-public class T {
-    public static void main(String[] args) {
-        // 实例窗口类
-        Frame f = new Frame();
-
-        // 设置窗口大小
-        f.setSize(800, 600);
-
-        // 设置自适应窗口
-        f.setResizable(false);
-
-        // 设置标题
-        f.setTitle("tank war");
-
-        // 窗口显示
-        f.setVisible(true);
-
-        // 监听窗口
-        // WindowAdapter 是 WindowListener 的子类
-        f.addWindowListener(new WindowAdapter() {
-            // 实现窗口关闭
-            @Override
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-
-
-        });
-    }
-}
-*/
--- src/com/tryboy/tank/TankFrame.java
+++ src/com/tryboy/tank/TankFrame.java
@@ -1,6 +1,8 @@
 package com.tryboy.tank;
 
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -20,6 +22,9 @@ public class TankFrame extends Frame {
 
         // 窗口显示
         setVisible(true);
+
+        // 键盘监听
+        addKeyListener(new MykeyListener());
 
         // 监听窗口
         // WindowAdapter 是 WindowListener 的子类
@@ -41,5 +46,22 @@ public class TankFrame extends Frame {
         g.fillRect(x, y, 100, 100);
         x += 10;
         y += 10;
+    }
+
+    // 键盘监听事件处理实例类
+    class MykeyListener extends KeyAdapter {
+
+        // 键位弹起效果
+        @Override
+        public void keyReleased(KeyEvent e) {
+//            x += 20;
+//            repaint();
+        }
+
+        // 键位按压处理
+        @Override
+        public void keyPressed(KeyEvent e) {
+            System.out.println("key pressed");
+        }
     }
 }
